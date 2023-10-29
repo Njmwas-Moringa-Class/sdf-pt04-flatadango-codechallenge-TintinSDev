6f51ebe0bc6942e940be60e635837b23d0767837
Martin <mmaina290@gmail.com>
2023-10-29 17:27:52 +0300

    Buy ticket done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ async function fetchFilms() {
     const films = await filmsResponse.json()
     return fetch(`${FILMS_URL}/films`)
 }
+function subtract() {
+    tickets.textContent = tickets.textContent - 1
+    document.querySelector('#ticket-num').value = tickets.textContent;
+    tickets.onclick = subtract;
+    
+}
+
 
 const titleName = document.querySelector('#title');
 const runtime = document.querySelector('.meta');
@@ -15,6 +22,7 @@ const filmList = document.querySelector('#films');
 const showtime = document.querySelector('#showtime')
 const card = document.querySelector('#card');
 const posters = document.querySelector('#poster')
+const tickets = document.querySelector('#ticket-num')
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -25,18 +33,29 @@ document.addEventListener('DOMContentLoaded', () => {
             posters.src = films[0].poster
             titleName.innerText = films[0].title
             runtime.innerHTML = films[0].runtime
-            description.innerHTML = films[0].description
+            //  description.innerHTML = films[0].description
             filmList.innerHTML = ''
             for (let film of films) {
                 filmList.innerHTML += `<li>${film.title}</li>`
             }
             showtime.textContent = films[0].showtime
-
-            
+            tickets.innerHTML= films[0].tickets_sold    
         })
+      
+
+      
+        
         const buyTicket = document.getElementById('buy-ticket');
-        buyTicket.addEventListener('click', handleSubmit => {
-            handleSubmit.preventDefault();
+        buyTicket.addEventListener('click', (e) => {
+           e.preventDefault();
+            fetch(`${FILMS_URL}/films`)
+                .then((res) => res.json())
+                .then(films => {
+                    console.log(films)
+                })
+            subtract();
+
+        
             
         }) 
             filmList.addEventListener('click', (e) => { 
